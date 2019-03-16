c05d846455d83a275bf27537ac383043b731a0e6
miguel jimenez <denkatalabeno@gmail.com>
2019-03-16 20:23:06 +0100

    added copy functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,18 +2,18 @@ import "bootstrap";
 import ClipboardJS from 'clipboard/dist/clipboard.min';
 
 const input = document.querySelector('.query-input')
-const snippets = document.querySelectorAll('.card-text');
+const textSnippets = document.querySelectorAll('.card-body');
+const copyButtons = document.querySelectorAll('.copy-button');
+
+new ClipboardJS(copyButtons);
 const wordsArray = input.dataset.query.split(' ');
 console.log(wordsArray);
 
-const clipboard = new ClipboardJS('#id-test');
-console.log(clipboard);
 
-snippets.forEach((snippet) => {
 
-  console.log(input.dataset);
-  console.dir(snippet);
+// primitive text highlighter
 
+textSnippets.forEach((snippet) => {
   wordsArray.forEach((word) => {
     if (snippet.innerText.includes(word) ) {
       snippet.innerHTML = snippet.innerHTML.replace(word, `<i class="highlighted">${word}</i>`)
